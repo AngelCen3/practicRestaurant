4bcbdf83a4446246321875f501a4f7d2905f8ae5
BetoRobert <wyjawy@hotmail.com>
2024-02-07 01:42:11 -0500

    Aqui ya alineamos el menu a en la parte derecha de la pantalla

--- resources/views/livewire/google-fake.blade.php
+++ resources/views/livewire/google-fake.blade.php
@@ -5,8 +5,8 @@
             <ul class="nav-right-section">   <!--BEEN Arquitectur?! to name a class-->
                 <li><a href="">Gmail</a></li>
                 <li><a href="">Imágenes</a></li>
-                <li><a href="">Icon</a></li>
-                <li><a href="">Photo</a></li>
+                <li class="menu-icon"><a href=""></a></li>
+                <li><a href=""><img src="https://lh3.googleusercontent.com/ogw/ANLem4bOKv814qXUftu7-vAE0iLylKRZcnEwqe4KLnuY=s32-c-mo" alt="imagen profile"></a></li>
             </ul>
         </nav>
     </header>
