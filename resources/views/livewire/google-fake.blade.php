e94f28aed6db8e9789d9b886b8d20189702d1b87
BetoRobert <wyjawy@hotmail.com>
2024-02-06 23:28:56 -0500

    Aqui ya alineamos el menu a en la parte derecha de la pantalla

--- resources/views/livewire/google-fake.blade.php
+++ resources/views/livewire/google-fake.blade.php
@@ -1,13 +1,15 @@
 <div>
     {{-- ul>li*4>a The whole world belongs to you. --}}
     <header>
-
+        <nav>
+            <ul class="nav-right-section">   <!--BEEN Arquitectur?! to name a class-->
+                <li><a href="">Gmail</a></li>
+                <li><a href="">Imágenes</a></li>
+                <li><a href="">Icon</a></li>
+                <li><a href="">Photo</a></li>
+            </ul>
+        </nav>
     </header>
-    <ul>
-        <li><a href="">Gmail</a></li>
-        <li><a href="">Imágenes</a></li>
-        <li><a href="">Icon</a></li>
-        <li><a href="">Photo</a></li>
-    </ul>
+
 
 </div>
